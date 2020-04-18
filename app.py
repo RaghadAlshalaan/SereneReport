c471c716be7c01e37e410603a2b9995cc8cdda61
anoodr <anoodalrassan@gmail.com>
2020-04-18 22:12:47 +0300

    Revert "5"
    This reverts commit 7a486c1e21a3a139596e9a005d668b378678143c.

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)#referance the file
-@app.route("/home/<string:name><int:id>")
-
-def hello(name , id):
-    return {"Hello World!": name , "id":id}
-
-if __name__ == "__main__":
-    app.run(debug=True)
